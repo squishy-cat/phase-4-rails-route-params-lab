69f3959c0d33d1888d38e5b192a9a718090183a2
Eve Kozina <eve.kozina@gmail.com>
2022-01-10 15:36:37 -0500

    commit tree 89da81e434da5ea05d95b690c68f2751a995f87c

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,12 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.find(params[:first_name, :last_name])
+    render json: students
+  end
+
+  def show
+    students = Student.find(params[:id])
     render json: students
   end
 
